f26aafaa7af84ae2292466e1a6d4ebfecabbf7e9
Claire <87963348+inasnowstorm@users.noreply.github.com>
2024-10-24 11:41:07 +1000

    Changing gamma to delta

--- src/structs.jl
+++ src/structs.jl
@@ -7,8 +7,8 @@ module structs
         id::Int64                               # Identity number to keep track of individual firms
         alpha::Float64                          # Learning parameter
         beta::Float64                           # Randomisation parameter
-        gamma::Float64                          # Discount factor
-        mc::Float64                              # Marginal cost
+        delta::Float64                          # Discount factor
+        mc::Float64                             # Marginal cost
         k::Int64                                # Dimension of the price grid
         location::Float64                       # Location on circle hotelling model
 
@@ -23,7 +23,7 @@ module structs
     mutable struct consumer
         """set properties"""
         location::Float64                       # Location on circle hotelling model
-        v::Float64                                # Value of the product
+        v::Float64                              # Value of the product
         mu::Float64                             # Product differentiation/travelling cost
 
         """memory"""
